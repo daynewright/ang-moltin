da9709734a93aff3fa0ef933fb1777507fa1b8f6
Dayne Wright <daynewr@gmail.com>
2016-09-08 10:31:32 -0500

    updated local cart structure for delete

--- app/controllers/Cart.js
+++ app/controllers/Cart.js
@@ -1,18 +1,26 @@
 'use strict';
 
 app.controller('CartCtrl', function($scope, $route, cart, moltin, $location){
+
+  for(let item in cart.contents){
+    cart.contents[item].uid = item;
+  }
+
   $scope.cart = cart;
 
-  $scope.delete = (id)=> {
-      moltin.Cart.Remove(id, function(){
-        console.log('Item deleted from cart');
-        $scope.cart = cart;
-      }, function(error){
-        console.error('This happened: ', error);
-      });
+  $scope.delete = (id, uid)=> {
+    moltin.Cart.Remove(id, function(){
+      delete cart.contents[uid];
+      if(Object.keys(cart.contents).length === 0){
+          cart.total_items = 0;
+      }
+      $scope.cart = cart;
+      $scope.$apply();
+    }, function(error){
+      console.error('This happened: ', error);
+    });
   };
 
-  console.log(cart);
-
   $scope.activeTab = $route.current.$$route.activetab;
+
 });
